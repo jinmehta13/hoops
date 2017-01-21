11c52b26140199befd212015d5323751ebeb8e96
jezreel <jezreeljardeleza@gmail.com>
2017-01-21 15:23:02 -0500

    server is functional on localhost:8080

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-//include dependencies
 var mongoose = require('mongoose');
 var nodemon = require('nodemon');
-var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 
+var express = require('express');
+var app     = express();
+var port    =  8080;
 
-var router = express.Router();
+// ROUTES
+// ==============================================
+
+// sample route with a route the way we're used to seeing it
+app.get('/', function(req, res) {
+    res.send('hello world!');  
+});
+
+// we'll create our routes here
+
+// START THE SERVER
+// ==============================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
