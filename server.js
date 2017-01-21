5097c82fef5ca8ca59051407b6c352ed21f71834
jezreel <jezreeljardeleza@gmail.com>
2017-01-21 15:35:01 -0500

    port error fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var express = require('express');
 var app     = express();
-var port    =  8080;
+var port    =  process.env.PORT || 8080;
 
 // ROUTES
 // ==============================================
