51a6d7946deb61899149b10e155a4030d1b2e9bc
jezreel <jezreeljardeleza@gmail.com>
2017-01-21 14:48:27 -0500

    added dependencies

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
+//include dependencies
 var mongoose = require('mongoose');
+var nodemon = require('nodemon');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
 
