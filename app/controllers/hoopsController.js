a35faed3bf3649d6ee757534ae87a3ac1f65c705
jezreel <jezreeljardeleza@gmail.com>
2017-01-22 18:37:56 -0500

    added a test landing page

--- app/controllers/hoopsController.js
+++ app/controllers/hoopsController.js
@@ -2,5 +2,6 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('hoopsController', ['$scope', function($scope) {
   $scope.test = 'Hello world, AngularJS is working as expected';
+  $scope.items = [ 1, 2, 3, 4, 5, 6];
 }]);
 
