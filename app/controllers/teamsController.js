71ef203f6b11971a1f61aaaa1ea92148946ab896
jezreel <jezreeljardeleza@gmail.com>
2017-01-24 20:15:59 -0500

    added navbar

--- app/controllers/teamsController.js
+++ app/controllers/teamsController.js
@@ -1,6 +1,6 @@
 
 myApp.controller('teamsController', ['$scope', function($scope) {
   $scope.test = 'Hello world, AngularJS is working as expected';
-  $scope.items = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32];
+  $scope.items = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 }]);
 
