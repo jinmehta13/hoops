a7bab79d2a305985069cf6d99fa7c652eef7ff29
jezreel <jezreeljardeleza@gmail.com>
2017-01-25 19:15:23 -0500

    added logo images to the home screen

--- app/controllers/teamsController.js
+++ app/controllers/teamsController.js
@@ -1,6 +1,38 @@
 
 myApp.controller('teamsController', ['$scope', function($scope) {
   $scope.test = 'Hello world, AngularJS is working as expected';
-  $scope.items = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-}]);
+  $scope.teams = [
+			{"teamGif": "76ers.gif"},
+			{"teamGif": "bucks.gif"},
+			{"teamGif": "bulls.gif"},
+			{"teamGif": "cavaliers.gif"},
+			{"teamGif": "grizzlies.gif"},
+			{"teamGif": "hawks.gif"},
+			{"teamGif": "heat.gif"},
+			{"teamGif": "hornets.gif"},
+			{"teamGif": "jazz.gif"},
+			{"teamGif": "kings.gif"},
+			{"teamGif": "knicks.gif"},
+			{"teamGif": "lakers.gif"},
+			{"teamGif": "magic.gif"},
+			{"teamGif": "mavericks.gif"},
+			{"teamGif": "nets.gif"},
+			{"teamGif": "nuggets.gif"},
+			{"teamGif": "pacers.gif"},
+			{"teamGif": "pelicans.gif"},
+			{"teamGif": "pistons.gif"},
+			{"teamGif": "raptors.gif"},
+			{"teamGif": "rockets.gif"},
+			{"teamGif": "spurs.gif"},
+			{"teamGif": "suns.gif"},
+			{"teamGif": "thunder.gif"},
+			{"teamGif": "timberwolves.gif"},
+			{"teamGif": "trailblazers.gif"},
+			{"teamGif": "warriors.gif"},
+			{"teamGif": "wizards.gif"}
+			];
+		}		
+]);
+		
+
 
